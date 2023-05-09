c9da6ec83b892bca7f4373889d6f5be58b51d931
citkniit <aleksandr200411y@mail.ru>
2023-05-09 14:22:37 +0300

    Рекурсивный вывод n-ого числа фибоначи, числа начинаются с 0,1...

--- vvv.cpp
+++ vvv.cpp
@@ -3,6 +3,10 @@ using namespace std;
 int fib(n){
 if (n == 1 || n == 2) return 1;
 else return fib(n-1) + fin(n-2); 
+if (n == 1) return 0;
+else if (n == 2) return 1;
+else return fib(n-1) + fib(n-2);
+} 
 int main(){
 int n;
 cin >> n;

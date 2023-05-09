86f3d357647c5c656812595ffec2e48dfa991721
citkniit <aleksandr200411y@mail.ru>
2023-05-09 14:24:25 +0300

    Итеративный вывод первых n чисел фибоначи

--- vvv.cpp
+++ vvv.cpp
@@ -2,15 +2,36 @@
 using namespace std;
 int fib(n){
 if (n == 1 || n == 2) return 1;
-else return fib(n-1) + fin(n-2); 
 if (n == 1) return 0;
 else if (n == 2) return 1;
 else return fib(n-1) + fib(n-2);
-} 
-int main(){
-int n;
+@@ -11,4 +10,28 @@ int n;
 cin >> n;
 cout << fib(n);
 return 0;
+int fib(int n)
+{
+    if (n == 1) {
+        return 0;
+ }
+    if (n == 2) {
+        return 1;
+    }
+    int n0 = 0;
+    int n1 = 1;
+    for (int i = 3; i <= n; i++) {
+        int n2 = n0 + n1;
+        n0 = n1;
+        n1 = n2;
+    }
+    return n1;
 }
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cout << fib(i) << endl;
+    }
+    return 0;
 
